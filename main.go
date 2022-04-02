e06ebebb44acb3238f98165fef9b58a1ff60a9e3
joelei <thezero12@hotmail.com>
2022-04-03 01:09:55 +0800

    Add: defaultConfig

--- main.go
+++ main.go
@@ -1,30 +1,27 @@
 package main
 
-// Package is called aw
 import (
 	aw "github.com/deanishe/awgo"
 
 	"github.com/ifooth/alfred-ssh-workflow/config"
 )
 
-// Workflow is the main API
+const (
+	defaultConfig = "~/.config/sshmgr/config.yml"
+)
+
 var (
 	wf  *aw.Workflow
 	cfg *aw.Config
 )
 
 func init() {
-	// Create a new Workflow using default settings.
-	// Critical settings are provided by Alfred via environment variables,
-	// so this *will* die in flames if not run in an Alfred-like environment.
 	wf = aw.New()
 	cfg = aw.NewConfig()
 }
 
-// Your workflow starts here
 func run() {
-	conf_path := cfg.GetString("config")
-	conf, err := config.ReadConfig(conf_path)
+	conf, err := config.ReadConfig(cfg.GetString("config", defaultConfig))
 	if err != nil {
 		panic(err)
 	}
@@ -40,7 +37,5 @@ func run() {
 }
 
 func main() {
-	// Wrap your entry point with Run() to catch and log panics and
-	// show an error in Alfred instead of silently dying
 	wf.Run(run)
 }
